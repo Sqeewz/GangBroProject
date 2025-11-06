37dc6f98f5dae5bf972e012f2b26a7c7e3b7894d
Sqeewz <dotheripper1@gmail.com>
2025-11-06 19:41:49 +0700

    Implement configuration loading and environment variable handling

--- src/config/config_loader.rs
+++ src/config/config_loader.rs
@@ -1,3 +1,31 @@
-#[derive(Debug, Clone, Default, PartialEq)]
+use anyhow::Result;
 
-pub enum Stage{}
+use crate::config::{config_model::{Database, DotEnvyConfig, Server}, stage::Stage};
+
+pub fn load() -> Result<DotEnvyConfig> {
+    dotenvy::dotenv().ok();
+
+    let server = Server(std::env::var("SERVER_PORT").expect("SERVER_PORT not set").parse()?, std::env::var("SERVER_BODY_LIMIT").expect("SERVER_BODY_LIMIT not set").parse()?, std::env::var("SERVER_TIMEOUT").expect("SERVER_TIMEOUT not set").parse()?);
+
+    let database = Database {
+        url: std::env::var("DATABASE_URL").expect("DATABASE_URL not set"),
+    };
+
+    let secret = std::env::var("JWT_SECRET").expect("JWT_SECRET not set");
+
+    let config = DotEnvyConfig {
+        server,
+        database,
+        secret,
+    };
+
+    Ok(config)
+}
+
+pub fn get_stage() ->Stage {
+    dotenvy::dotenv().ok();
+
+    let stage_str = std::env::var("STAGE").unwrap_or_else(|_| "".to_string());
+
+    Stage::from_str(&stage_str).unwrap_or_default()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use server::config::config_loader::load;
+use tracing::{Level, info};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    let dotenvy_env = match load() {
+        Ok(env) => env,
+        Err(e) => {
+            tracing::error!("Failed to load .env file: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("YESSSSS");
+    info!("Loaded configuration: {:?}", dotenvy_env);           
+
 }
